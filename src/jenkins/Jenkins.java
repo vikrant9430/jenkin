53aa26d2012622c69cd2d045f9a2a836ecfde59b
Vikrant Nandan <vikrant1089@gmail.com>
2018-01-28 20:01:30 +0530

    First Commit

---
+++ src/jenkins/Jenkins.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Jenkins!!");
+
+	}
+
+}
